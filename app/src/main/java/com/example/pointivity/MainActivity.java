c62f46e6fa40f03fe988be3ed1907b0d6a2db127
masan <mnakura@uw.edu>
2022-07-23 16:40:48 -0700

    Created a profile screen

--- app/src/main/java/com/example/pointivity/MainActivity.java
+++ app/src/main/java/com/example/pointivity/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.pointivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test
+        // making the MainActivity full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // listen for profile button
+        findViewById(R.id.profile).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                // start the game
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            }
+        });
     }
 }
