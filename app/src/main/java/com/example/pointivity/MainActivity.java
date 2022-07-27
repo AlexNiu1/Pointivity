27d87f7b4b290b5733949211d5152398a2831108
AlexNiu <alexniupdx@gmail.com>
2022-07-26 18:23:35 -0700

    added timer

--- app/src/main/java/com/example/pointivity/MainActivity.java
+++ app/src/main/java/com/example/pointivity/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.pointivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.OnLifecycleEvent;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.WindowManager;
-import android.content.SharedPreferences;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +48,48 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // start the game
                 startActivity(new Intent(MainActivity.this, ShopActivity.class));
+            }
+        });
+
+        TextView mTextField = (TextView) findViewById(R.id.infotext);
+        EditText mMinutes = (EditText) findViewById(R.id.minutes);
+        Button mButton = (Button) findViewById(R.id.button);
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mButton.setEnabled(false);
+                mButton.setClickable(false);
+                mMinutes.setEnabled(false);
+                int start;
+                if (mMinutes.getText().toString().isEmpty()){
+                    start = 5;
+                }
+                else {
+                    start = Integer.parseInt(mMinutes.getText().toString());
+                }
+                if (start < 5){
+                    start = 5;
+                }
+
+                CountDownTimer timer = new CountDownTimer(start * 60000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        mTextField.setText("Time remaining:" + millisUntilFinished / 60000 + " minutes, " + (millisUntilFinished % 60000)/1000 + "seconds");
+                    }
+
+                    public void onFinish() {
+                        mTextField.setText("Focus Mode Complete, congratulations");
+                        mMinutes.setText("");
+                        mButton.setEnabled(true);
+                        mButton.setClickable(true);
+                        mMinutes.setEnabled(true);
+                    }
+                };
+                timer.start();
+
+
             }
         });
     }
