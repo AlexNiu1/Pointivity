6da610489cc32f1826004d915c829526e5987f93
AlexNiu <alexniupdx@gmail.com>
2022-07-28 19:59:52 -0700

    added back buttons

--- app/src/main/java/com/example/pointivity/ProfileActivity.java
+++ app/src/main/java/com/example/pointivity/ProfileActivity.java
@@ -1,8 +1,12 @@
 package com.example.pointivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.content.SharedPreferences;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +20,13 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         SharedPreferences prefs = getSharedPreferences("app", MODE_PRIVATE);
         int points = prefs.getInt("points", 0);
+        TextView homebutton = findViewById(R.id.homebutton2);
+        homebutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                // start the game
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/pointivity/ShopActivity.java
+++ app/src/main/java/com/example/pointivity/ShopActivity.java
@@ -1,8 +1,12 @@
 package com.example.pointivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.content.SharedPreferences;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +26,13 @@ public class ShopActivity extends AppCompatActivity {
         editor.putInt("points", (int) points - itemCost);
         editor.apply();
          */
+        TextView homebutton = findViewById(R.id.homebutton);
+        homebutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                // start the game
+                startActivity(new Intent(ShopActivity.this, MainActivity.class));
+            }
+        });
     }
 
 }
